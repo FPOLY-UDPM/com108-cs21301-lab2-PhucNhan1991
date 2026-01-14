e8ca9c6ef2389309694b759cb2fff880219caad1
PhucNhan1991 <phucnhan27081991@gmail.com>
2026-01-14 19:17:54 +0700

    Chỉnh sửa bài 5

--- bai5.c
+++ bai5.c
@@ -1,31 +1,27 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Phúc Nhân]
+ * MSSV:      [PS47835]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 // BÀI LÀM THÊM
 // BÀI 5: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TÍCH VÀ THƯƠNG CỦA 2 SỐ
 
-#include <stdio.h>
-
-int main(){
-    int so1, so2;
-    printf("Nhập 2 số 1:");
-    scanf("%d",&so1);
-
-    printf("Nhập 2 số 2:");
-    scanf("%d",&so2);
-
-    int tich=1;
-    float thuong;
-
-    tich = so1 * so2;
-    thuong = so1 /so2;
-    printf(" tích của 2 số=%d\n",tich);
-    printf("Thương của 2 số =%.2f8 ",thuong);
-
-
-
+#include<stdio.h>
+float a, b, thuong;
+int main()
+{
+    printf("Nhập 2 số a và b :");
+    scanf("%f %f",&a, &b);
+    if(b == 0)
+    {
+        printf("Phép chia không thể thực hiện do b = 0");
+    }
+    else 
+    {
+        thuong = a/b;
+        printf("Kết quả của %.2f/%.2f = %.2f",a,b,thuong);
+    }
     return 0;
-} 
+}
+
